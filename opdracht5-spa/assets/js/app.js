e4de069ef8c5f1a51592933589dd737a87ab72b9
Elton Goncalves <elton@XS2-MacBook-Pro.local>
2017-02-12 12:54:52 +0100

    the app checks send users to the initRoute if the current hash doesn't exist

--- opdracht5-spa/assets/js/app.js
+++ opdracht5-spa/assets/js/app.js
@@ -1,155 +1,135 @@
-( function() {
+( function(global) {
     "use strict";
 
     // Generates html content for Page class
-    var helpers = {
-        createSection: function(options){
-            var mainContainer = document.querySelector('main');
-
-        	let { element, id, childrenArray } = options;
-
-        	var parent = document.createElement(element);
-        	var children;
-
-        	for (var x in options) {
-        		if (typeof options[x] !== "object") {
-        			parent[x] = options[x];
-        		}
-        	}
-
-        	//
-        	if (childrenArray) {
-        		children = childrenArray.map((item)=>{
-        			var child;
-        			child = document.createElement(item.element);
-
-        			if (item.innerHMTL) {
-        				child.innerHTML = item.innerHMTL;
-        			}
-        			if (item.className) {
-        				child.className = item.className;
-        			}
-        			if (item.id) {
-        				child.id = item.id;
-        			}
-        			if (item.event) {
-        				child.addEventListener("click", item.event);
-        			}
-        			return child;
-        		});
-
-
-        		for (var i = 0; i < children.length; i++) {
-        			parent.appendChild(children[i]);
-        		}
-        	}
+    let helpers = {
+        createSection: function(props){
+            // Where everything will be appended to
+            let mainContainer = document.querySelector('main');
+            // get properties from the parameter, less typing
+            let { element, childrenArray } = props;
+            // create a HTML element based on given properties
+            let parent = document.createElement(element);
+            // loops through all the properties, skips all but objects
+            for (let x in props) {
+                if (typeof props[x] !== "object") {
+                    parent[x] = props[x];
+                }
+            }
+            // Builds children if there is a childrenArray and appends it to a parent
+            if (childrenArray) {
+                childrenArray.forEach((item)=>{
+                    console.log(item);
+                    let child = document.createElement(item.element);
+                    for (let x in item) {
+                            child[x] = item[x];
+                    }
+                    parent.appendChild(child)
+                })
+            }
+            // Appends parent to the main element
             mainContainer.appendChild(parent);
-            // add this item to the nav
+            // Adds this item as a link in the navigation
             this.createNavElement(parent.id, parent.className);
-        	return parent;
+            return parent;
         },
         createNavElement: function(id, className){
-            var nav = document.querySelector('nav ul');
-
-            var li = document.createElement("li");
-            var a = document.createElement("a");
+            // Selects the nav element
+            let nav = document.querySelector('nav ul');
+            // Appends a new li to the nav
+            let li = document.createElement("li");
+            let a = document.createElement("a");
             a.innerText = id;
             a.href= `#${id}`;
 
             li.appendChild(a);
-
-            console.dir(a);
             nav.appendChild(li);
+        },
+        showSelectedRoute: function(route) {
+            // Gets the array that is stored in global window object
+            let { sections } = global;
+            // I replaced the for loop with a nice clean forEach Function - Dave Bitter
+            sections.forEach(function (section) {
+                // Hide all sections
+                section.classList.add("hidden");
+            });
+            // Show the selected route
+            route.classList.remove("hidden");
+        },
+        checkExistingRoute: function(hash) {
+            // Gets the array that is stored in global window object
+            let { sections } = global
+            // Turns an id into a #-less string
+            let replacedHash = hash.replace("#","");
+            let route, initRoute = "#home";
+            // Loop through all sections and checks if current hash exists
+            sections.forEach((section)=>{
+                if (section.id == replacedHash) {
+                    route = location.hash
+                }
+            })
+            // The user sees initRoute if the hash doesn't exist
+            route == null || route == "" ? route = initRoute : route;
 
-
+            return document.querySelector(route);
         }
     };
 
     // Page class
-    var Pages = function(pagename){
-        var pageContent = "Newly generated page";
-        var htmlElement = helpers.createSection({
+    let Pages = function(pagename, pageContent){
+        let htmlElement = helpers.createSection({
             element:"section",
             id:pagename,
             childrenArray:[
-                {element:"h1", innerHMTL: pageContent}
+                {element:"h1", innerHTML: pageContent}
             ]
         });
 
     };
 
+    let options = (function(){
+        // TODO: make pageContent better
+        var newPage = new Pages("Page1", "Newly generated page");
+        self.sections = document.querySelectorAll('section');
+        return {
+            page1: newPage ,
+            sections: self.sections,
+            initRoute: helpers.checkExistingRoute(location.hash)
+        }
+    })();
 
-    var options = {
-        page1: new Pages("Page1"),
-        initRoute: document.querySelector("#home"),
-        sections: document.querySelectorAll('section'),
-    };
-
-
-    // start app
-    var app = {
+    // Function to setup the app
+    let app = {
         init: function(){
-
-
             routes.init();
         }
     };
 
-
-    // create routes
-    var routes = {
+    // Function to setup the routes
+    let routes = {
         init: function(){
-
-            console.log("Router initialized");
-            let { sections, initRoute } = options;
-
-            // I replaced the for loop with a nice clean forEach Function - Dave Bitter
-            sections.forEach(function (sec) {
-                sec.classList.add("hidden");
-            });
-
-            initRoute.classList.remove("hidden");
-
+            helpers.showSelectedRoute(options.initRoute)
             this.hashChange();
-
         },
         // change the route
-        hashChange: function(){
-            // source: https://developer.mozilla.org/nl/docs/Web/API/WindowEventHandlers/onhashchange
-            window.onhashchange = function() {
-
-                // No need for new var just for this, btw use let/const consistently - Dave Bitter
-                sections.toggle(location.hash);
-
-            };
+        hashChange: window.onhashchange = function() {
+            let  currentSection = helpers.checkExistingRoute(location.hash)
+            sections.toggle(currentSection);
         },
     };
 
-
     // handles the sections
-    var sections = {
+    let sections = {
         toggle: function(route){
-            let { sections } = options;
-
-            var selectedRoute = document.querySelector(route);
-            console.log(route);
-
-            // I replaced the for loop with a nice clean forEach Function - Dave Bitter
-            sections.forEach(function (sec) {
-                sec.classList.add("hidden");
-            });
-
-            selectedRoute.classList.remove("hidden");
+            helpers.showSelectedRoute(route)
             this.loadContent();
         },
-
         loadContent: function(){
             console.log("Content");
         }
     };
 
-
+    // Start the app
     app.init();
 
-
-})();
+})(this);
