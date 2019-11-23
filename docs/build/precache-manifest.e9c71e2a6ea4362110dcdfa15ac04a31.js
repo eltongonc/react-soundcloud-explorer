48edea9863e1452b03c7918d1783ae68c75b54b3
Elton Gonçalves Gomes <eltongonc@gmail.com>
2019-11-23 16:41:34 +0100

    added a docs folder

---
+++ docs/build/precache-manifest.e9c71e2a6ea4362110dcdfa15ac04a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d05028e175508aac17b434e6976da6",
+    "url": "/react-soundcloud-explorer/index.html"
+  },
+  {
+    "revision": "fe2cbf12627f59330654",
+    "url": "/react-soundcloud-explorer/static/css/main.3272e6b2.chunk.css"
+  },
+  {
+    "revision": "6fc39e189c4e9341d4e5",
+    "url": "/react-soundcloud-explorer/static/js/2.5fea68c8.chunk.js"
+  },
+  {
+    "revision": "fe2cbf12627f59330654",
+    "url": "/react-soundcloud-explorer/static/js/main.720cdbea.chunk.js"
+  },
+  {
+    "revision": "163347f9204621f2783a",
+    "url": "/react-soundcloud-explorer/static/js/runtime-main.9ba54731.js"
+  }
+]);
